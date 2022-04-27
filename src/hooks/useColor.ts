d189360b7421070a32ec71ca66f373afa24b67f6
kwooshung <kwooshung@qq.com>
2022-04-27 15:53:52 +0800

    颜色转换搞定

--- src/hooks/useColor.ts
+++ src/hooks/useColor.ts
@@ -1,6 +1,6 @@
 import { useMemo, useState } from 'react';
 import { Colors } from '../../src/interfaces/Colors'
-import colorConvert from 'color-convert';
+import tinycolor from 'tinycolor2';
 
 export interface Actions {
     /**
@@ -16,18 +16,9 @@ export interface Actions {
      * @param {number} r 红
      * @param {number} g 绿
      * @param {number} b 蓝
+     * @param {number} [a=1] alpha 透明度 百分比
      */
-    setRgb: (r: number, g: number, b: number) => void;
-
-    /**
-     * 设置：RGB颜色
-     * 
-     * @param {number} r 红
-     * @param {number} g 绿
-     * @param {number} b 蓝
-     * @param {number} a alpha 透明度 百分比
-     */
-    setRgba: (r: number, g: number, b: number, a: number) => void;
+    setRgb: (r: number, g: number, b: number, a?: number) => void;
 
     /**
      * 设置：HSL颜色
@@ -35,105 +26,65 @@ export interface Actions {
      * @param {number} h 色相
      * @param {number} s 饱和度
      * @param {number} l 亮度
+     * @param {number} [a=1] alpha 透明度 百分比
      */
-    setHsl: (h: number, s: number, l: number) => void;
-
-    /**
-     * 设置：HSL颜色
-     * 
-     * @param {number} h 色相
-     * @param {number} s 饱和度
-     * @param {number} l 亮度
-     * @param {number} a alpha 透明度 百分比
-     */
-    setHsla: (h: number, s: number, l: number, a: number) => void;
-
-    /**
-     * 设置：HSB颜色
-     * 
-     * @param {number} h 色相
-     * @param {number} s 饱和度
-     * @param {number} b 亮度
-     */
-    setHsb: (h: number, s: number, b: number) => void;
-
-    /**
-     * 设置：HSB颜色
-     * 
-     * @param {number} h 色相
-     * @param {number} s 饱和度
-     * @param {number} b 亮度
-     * @param {number} a alpha 透明度 百分比
-     */
-    setHsba: (h: number, s: number, b: number, a: number) => void;
+    setHsl: (h: number, s: number, l: number, a?: number) => void;
 
     /**
      * 设置：HSV颜色
      * 
      * @param {number} h 色相
      * @param {number} s 饱和度
-     * @param {number} b 亮度
-     * @param {number} v alpha 透明度 百分比
+     * @param {number} v 亮度
+     * @param {number} [a=1] alpha 透明度 百分比
      */
-    setHsv: (h: number, s: number, v: number) => void;
-
-    /**
-     * 设置：HSV颜色
-     * 
-     * @param {number} h 色相
-     * @param {number} s 饱和度
-     * @param {number} b 亮度
-     * @param {number} v alpha 透明度 百分比
-     */
-    setHsva: (h: number, s: number, v: number, a: number) => void;
-
-    /**
-     * 设置：CMYK颜色
-     * 
-     * @param {number} c 青色、天蓝、湛蓝
-     * @param {number} m 品红、洋红
-     * @param {number} y 黄色
-     * @param {number} k 黑色
-     */
-    setCmyk: (c: number, m: number, y: number, k: number) => void;
-
-    /**
-     * 设置：CMYK颜色
-     * 
-     * @param {number} c 青色、天蓝、湛蓝
-     * @param {number} m 品红、洋红
-     * @param {number} y 黄色
-     * @param {number} k 黑色
-     * @param {number} v alpha 透明度 百分比
-     */
-    setCmyka: (c: number, m: number, y: number, k: number, a: number) => void;
+    setHsv: (h: number, s: number, b: number, a?: number) => void;
 };
 
 export default function useColors(defaultValue = false): [Colors, Actions] {
-    const [stateGet, stateSet] = useState<Colors>();
+    const [stateGet, stateSet] = useState<Colors>((() => {
+        const hex = '#000000';
+
+        return {
+            hex,
+            rgb: tinycolor(hex).toRgb(),
+            hsl: tinycolor(hex).toHsl(),
+            hsv: tinycolor(hex).toHsv()
+        };
+    })());
 
     const methods = {
+        /**
+         * 标准化十六进制颜色
+         *
+         * @param {string} hex 十六进制颜色
+         * @return {string} 标准十六进制颜色
+         */
+        hexStandard(hex: string): string {
+            hex = hex.replace(/^#{1,}/, '');
+            if (hex.length === 3) {
+                hex = `${hex[0]}${hex[0]}${hex[1]}${hex[1]}${hex[2]}${hex[2]}`;
+            }
+            else if (hex.length === 4) {
+                hex = `${hex[0]}${hex[0]}${hex[1]}${hex[1]}${hex[2]}${hex[2]}${hex[3]}${hex[3]}`;
+            }
+
+            return `#${hex}`;
+        },
         /**
          * 设置：十六进制颜色
          * 
          * @param {string} hex 颜色值
          */
         setHex(hex: string) {
-            const val: Colors['hex'] = hex;
+            hex = this.hexStandard(hex);
 
-            const _rgb = colorConvert.hex.rgb(hex);
-            const rgb: Colors['rgb'] = {
-                r: _rgb[0],
-                g: _rgb[1],
-                b: _rgb[2]
-            };
-
-            const rgba: Colors['rgb'] = {
-                r: _rgb[0],
-                g: _rgb[1],
-                b: _rgb[2],
-                a: 1
-            };
+            stateSet({
+                hex,
+                rgb: tinycolor(hex).toRgb(),
+                hsl: tinycolor(hex).toHsl(),
+                hsv: tinycolor(hex).toHsv()
+            });
         },
 
         /**
@@ -142,67 +93,35 @@ export default function useColors(defaultValue = false): [Colors, Actions] {
          * @param {number} r 红
          * @param {number} g 绿
          * @param {number} b 蓝
+         * @param {number} [a=1] alpha 透明度 百分比
          */
-        setRgb(r: number, g: number, b: number) {
+        setRgb(r: number, g: number, b: number, a: number = 1) {
+            const _rgb: Colors['rgb'] = { r, g, b, a };
 
+            stateSet({
+                hex: a === 1 ? tinycolor(_rgb).toHex() : tinycolor(_rgb).toHex8(),
+                rgb: tinycolor(_rgb).toRgb(),
+                hsl: tinycolor(_rgb).toHsl(),
+                hsv: tinycolor(_rgb).toHsv()
+            });
         },
-
-        /**
-         * 设置：RGB颜色
-         * 
-         * @param {number} r 红
-         * @param {number} g 绿
-         * @param {number} b 蓝
-         * @param {number} a alpha 透明度 百分比
-         */
-        setRgba(r: number, g: number, b: number, a: number) {
-
-        },
-
         /**
          * 设置：HSL颜色
          * 
          * @param {number} h 色相
          * @param {number} s 饱和度
          * @param {number} l 亮度
+         * @param {number} [a=1] alpha 透明度 百分比
          */
-        setHsl(h: number, s: number, l: number) {
+        setHsl(h: number, s: number, l: number, a: number = 1) {
+            const _hsl: Colors['hsl'] = { h, s, l, a };
 
-        },
-
-        /**
-         * 设置：HSL颜色
-         * 
-         * @param {number} h 色相
-         * @param {number} s 饱和度
-         * @param {number} l 亮度
-         * @param {number} a alpha 透明度 百分比
-         */
-        setHsla(h: number, s: number, l: number, a: number) {
-
-        },
-
-        /**
-         * 设置：HSB颜色
-         * 
-         * @param {number} h 色相
-         * @param {number} s 饱和度
-         * @param {number} b 亮度
-         */
-        setHsb(h: number, s: number, b: number) {
-
-        },
-
-        /**
-         * 设置：HSB颜色
-         * 
-         * @param {number} h 色相
-         * @param {number} s 饱和度
-         * @param {number} b 亮度
-         * @param {number} a alpha 透明度 百分比
-         */
-        setHsba(h: number, s: number, b: number, a: number) {
-
+            stateSet({
+                hex: a === 1 ? tinycolor(_hsl).toHex() : tinycolor(_hsl).toHex8(),
+                rgb: tinycolor(_hsl).toRgb(),
+                hsl: tinycolor(_hsl).toHsl(),
+                hsv: tinycolor(_hsl).toHsv()
+            });
         },
 
         /**
@@ -210,65 +129,27 @@ export default function useColors(defaultValue = false): [Colors, Actions] {
          * 
          * @param {number} h 色相
          * @param {number} s 饱和度
-         * @param {number} b 亮度
-         * @param {number} v alpha 透明度 百分比
+         * @param {number} v 亮度
+         * @param {number} [a=1] alpha 透明度 百分比
          */
-        setHsv(h: number, s: number, v: number) {
+        setHsv(h: number, s: number, v: number, a: number = 1) {
+            const _hsv: Colors['hsv'] = { h, s, v, a };
 
-        },
-
-        /**
-         * 设置：HSV颜色
-         * 
-         * @param {number} h 色相
-         * @param {number} s 饱和度
-         * @param {number} b 亮度
-         * @param {number} v alpha 透明度 百分比
-         */
-        setHsva(h: number, s: number, v: number, a: number) {
-
-        },
-
-        /**
-         * 设置：CMYK颜色
-         * 
-         * @param {number} c 青色、天蓝、湛蓝
-         * @param {number} m 品红、洋红
-         * @param {number} y 黄色
-         * @param {number} k 黑色
-         */
-        setCmyk(c: number, m: number, y: number, k: number) {
-
-        },
-
-        /**
-         * 设置：CMYK颜色
-         * 
-         * @param {number} c 青色、天蓝、湛蓝
-         * @param {number} m 品红、洋红
-         * @param {number} y 黄色
-         * @param {number} k 黑色
-         * @param {number} v alpha 透明度 百分比
-         */
-        setCmyka(c: number, m: number, y: number, k: number, a: number) {
-
+            stateSet({
+                hex: a === 1 ? tinycolor(_hsv).toHex() : tinycolor(_hsv).toHex8(),
+                rgb: tinycolor(_hsv).toRgb(),
+                hsl: tinycolor(_hsv).toHsl(),
+                hsv: tinycolor(_hsv).toHsv()
+            });
         }
     };
 
     const actions: Actions = useMemo(() => {
-
         return {
             setHex: methods.setHex,
             setRgb: methods.setRgb,
-            setRgba: methods.setRgba,
             setHsl: methods.setHsl,
-            setHsla: methods.setHsla,
-            setHsb: methods.setHsb,
-            setHsba: methods.setHsba,
-            setHsv: methods.setHsv,
-            setHsva: methods.setHsva,
-            setCmyk: methods.setCmyk,
-            setCmyka: methods.setCmyka
+            setHsv: methods.setHsv
         };
     }, []);
 
--- src/interfaces/colors.ts
+++ src/interfaces/colors.ts
@@ -2,9 +2,7 @@ export interface Colors {
     hex: string;
     rgb: ColorRGBA;
     hsl: ColorHSLA;
-    hsb: ColorHSBA;
     hsv: ColorHSVA;
-    cmyk: ColorCMYKA;
 }
 
 interface ColorRGBA {
@@ -20,23 +18,10 @@ interface ColorHSLA {
     l: number;
     a?: number;
 }
-interface ColorHSBA {
-    h: number;
-    s: number;
-    b: number;
-    a?: number;
-}
 
 interface ColorHSVA {
     h: number;
     s: number;
     v: number;
-    a?: number;
-}
-interface ColorCMYKA {
-    c: number;
-    m: number;
-    y: number;
-    k: number;
     a?: number;
 }
